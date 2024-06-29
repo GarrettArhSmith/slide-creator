6d8c91d2a98cc4a18ccaa3ce37894ad3f4580684
Garrett Smith <garrettarhsmith@gmail.com>
2024-06-29 11:43:23 -0400

    started working on mobile hamburger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       className='flex gap-10 min-h-screen flex-col items-center
       sm: p-10 lg:p-24'
     >
-      <h1 className='text-6xl font-extrabold mt-36 text-center bg-gradient-to-r from-fuchsia-600 to-violet-500 text-transparent bg-clip-text p-2'>Catchy SaaS Headline</h1>
+      <h1 className='text-6xl font-extrabold mt-16 text-center bg-gradient-to-r from-fuchsia-600 to-violet-500 text-transparent bg-clip-text p-2'>Catchy SaaS Headline</h1>
       <p className='max-w-3xl text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eaque sequi officiis!</p>
       <button className='hover:bg-violet-600 bg-violet-500 px-6 py-3 rounded-lg text-white transition'>
         Join the Wait List
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link'
 import { v4 as uuidv4 } from 'uuid';
 import ThemeSelector from './ThemeSelector';
-import { Bars3Icon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 type Props = {}
 
@@ -25,15 +25,23 @@ const Nav = (props: Props) => {
   }, [top]);
 
   return (
-    <header className={`sticky top-0 w-full mx-auto transition
-      ${!top && 'shadow-lg backdrop-blur-md bg-gradient-to-br from-white/5 to-white/0'}`}
+    <>
+    <div className={`${(top && open) && 'h-24'} ${(!top && open) && 'hidden'}`}></div>
+    <header className={`top-0 w-full mx-auto transition-all
+      ${!top && 'sticky shadow-lg backdrop-blur-md bg-gradient-to-br from-white/5 to-white/0'}
+      ${open && 'absolute shadow-lg backdrop-blur-md bg-gradient-to-br from-white/5 to-white/0'}`}
     >
       <nav className={
-        `flex max-w-7xl flex-row items-center justify-between mx-auto h-24 px-10`
+        `flex max-w-7xl flex-row items-center justify-between mx-auto box-border px-10 transition-all
+        ${open ? 'flex-col md:flex-row' : 'h-24'}`
       }>
-        <h1 className='font-bold text-xl font-mono'>LOGO</h1>
-        <Bars3Icon className='size-8 md:hidden' onClick={() => setOpen(prev => !prev)} />
-        <div className='hidden md:flex gap-10 items-center'>
+        <div className={`flex justify-between items-center w-full h-24`}>
+          <h1 className='font-bold text-xl font-mono'>LOGO</h1>
+          {open ?
+          <XMarkIcon className='size-8 md:hidden' onClick={() => setOpen(prev => !prev)} /> :
+          <Bars3Icon className='size-8 md:hidden' onClick={() => setOpen(prev => !prev)} />}
+        </div>
+        <div className={`${!open ? 'hidden md:flex' : 'flex-col-reverse pt-5 pb-10'} flex md:flex-row gap-10 items-center`}>
           <ThemeSelector />
           {routes.map(route => (
             <Link
@@ -42,12 +50,13 @@ const Nav = (props: Props) => {
               className='hover:text-violet-500 hover:transition'
             >{route.label}</Link>
           ))}
-          <button className='hover:bg-violet-600 transition bg-violet-500 px-4 py-2 rounded-lg text-white'>
+          <button className='hover:bg-violet-600 transition bg-violet-500 px-4 py-2 rounded-lg text-white text-nowrap'>
             Get Started
           </button>
         </div>
       </nav>
     </header>
+    </>
   )
 }
 
